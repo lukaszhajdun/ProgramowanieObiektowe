6068af38d91751513b32c4716f6216e94deaf225
lukaszhajdun <lukasz.hajdun@gmail.com>
2017-08-31 20:08:56 +0200

    Poprawiono Main w AuthorAndBook

--- OOP/src/AuthorAndBook/Main.java
+++ OOP/src/AuthorAndBook/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Author tomasNore = new Author("Tomas Nore", "tomas.nore@gmail.com",'m');
         System.out.println(tomasNore);
-        
+
         Book bookNight = new Book("Night",tomasNore,23.99,90);
         System.out.println(bookNight);
+
+        //CO GDY BEDZIE WIECEJ AUTORÓW
     }
 }
