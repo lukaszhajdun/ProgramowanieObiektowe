f37d73e00d770ef6890697e6dffca8b032c60303
lukaszhajdun <lukasz.hajdun@gmail.com>
2017-08-31 18:53:38 +0200

    Dodano Main do Date

--- OOP/src/Date/Main.java
+++ OOP/src/Date/Main.java
@@ -2,7 +2,7 @@ package Date;
 
 public class Main {
     public static void main(String[] args) {
-        Date date = new Date(01,12,1999);
+        Date date = new Date(0,0,0);
         System.out.println(date);
     }
 }
