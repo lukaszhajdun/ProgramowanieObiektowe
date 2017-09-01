716039fb49686b10785057731a23c4b6dbd3b689
lukaszhajdun <lukasz.hajdun@gmail.com>
2017-09-01 18:15:46 +0200

    Dodano CircleAndCylinder (dziedziczenie)

---
+++ OOP/src/CircleAndCylinder/Cylinder.java
@@ -0,0 +1,31 @@
+package CircleAndCylinder;
+
+public class Cylinder extends Circle {
+
+    private double _height = 1.0;
+
+    public Cylinder() {
+        super();
+    }
+    public Cylinder(double radius){
+        super(radius);
+    }
+    public Cylinder(double radius, double height){
+        super(radius);
+        _height = height;
+    }
+    public Cylinder(double radius, double height, String color){
+        super(radius, color);
+        double _height = height;
+    }
+    public double getHeight(){
+        return _height;
+    }
+    public void setHeight(double height){
+        _height = height;
+    }
+    public double getVolume(){
+        return super.getArea()*_height;
+    }
+
+}
